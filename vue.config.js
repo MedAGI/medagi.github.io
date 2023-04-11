6b1d8a8e5ad40f99f578ceccac022e84783cab1f
Byungju Chae <cbyungjub@naver.com>
2023-04-11 10:02:10 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'docs',
+  outputDir: './docs',
   publicPath: "/"
 })
