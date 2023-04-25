7db5e028cd77f1f2742b886b6598183c72ad182c
Byungju Chae <cbyungjub@naver.com>
2023-04-25 15:03:20 +0900

    Convert to WebHashHistory Mode

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: './docs',
-  publicPath: "/"
+  publicPath: "/",
+  devServer: { // 배포하는 서버에 옵션을 추가해야 함.
+    historyApiFallback: true // index.html로 fallback
+  }
 })
