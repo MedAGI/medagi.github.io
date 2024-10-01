d9a959363dce78e3b9789af322de43d9933d18b9
Jongha Kim <john0820693@gmail.com>
2024-10-01 19:15:27 +0900

    PDF loading in local dir

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,16 @@ module.exports = defineConfig({
   publicPath: "/",
   devServer: { // 배포하는 서버에 옵션을 추가해야 함.
     historyApiFallback: true // index.html로 fallback
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('pdf')
+      .test(/\.pdf$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: '[name].[ext]',
+        outputPath: 'assets/papers/'
+      });
   }
 })
